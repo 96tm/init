8de8c37dbb3ecf2fed81d77862a4d065b9371185
vjacheslav.s <vjacheslav.s@itfox-web.com>
2023-08-16 00:08:07 +0300

    feat: add sdk api clients

--- src/core/api/BuildClient.ts
+++ src/core/api/BuildClient.ts
@@ -1,13 +1,12 @@
 import fetch from 'node-fetch';
 import {
   ClientBuilder,
-
-  type AuthMiddlewareOptions, 
-  type HttpMiddlewareOptions, 
+  type PasswordAuthMiddlewareOptions,
+  type AuthMiddlewareOptions,
+  type HttpMiddlewareOptions,
+  Client,
 } from '@commercetools/sdk-client-v2';
-import {
-  createApiBuilderFromCtpClient,
-} from '@commercetools/platform-sdk';
+import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
 
 const projectKey = process.env.REACT_APP_PROJECT_KEY;
 const scopes = [process.env.REACT_APP_SCOPES];
@@ -29,11 +28,49 @@ const httpMiddlewareOptions: HttpMiddlewareOptions = {
 };
 
 export const ctpClient = new ClientBuilder()
-  .withProjectKey(projectKey) 
+  .withProjectKey(projectKey)
   .withClientCredentialsFlow(authMiddlewareOptions)
   .withHttpMiddleware(httpMiddlewareOptions)
-  .withLoggerMiddleware() 
+  .withLoggerMiddleware()
   .build();
 
-export const apiRoot = createApiBuilderFromCtpClient(ctpClient)
-  .withProjectKey({ projectKey: process.env.REACT_APP_PROJECT_KEY });
+export const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+  projectKey: process.env.REACT_APP_PROJECT_KEY,
+});
+
+const options: PasswordAuthMiddlewareOptions = {
+  host: process.env.REACT_APP_AUTH_URL,
+  projectKey: process.env.REACT_APP_PROJECT_KEY,
+  credentials: {
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+    user: {
+      username: '',
+      password: '',
+    },
+  },
+  scopes: scopes,
+  fetch,
+};
+
+let customerClient: null | Client = null;
+
+export let customerApiRoot: null | typeof apiRoot = null;
+
+export const buildClientWithPasswordFlow = (
+  username: string,
+  password: string
+) => {
+  const client = new ClientBuilder()
+    .withPasswordFlow({
+      ...options,
+      credentials: { ...options.credentials, user: { username, password } },
+    })
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .withLoggerMiddleware()
+    .build();
+  customerClient = client;
+  customerApiRoot = createApiBuilderFromCtpClient(
+    customerClient
+  ).withProjectKey({ projectKey: process.env.REACT_APP_PROJECT_KEY });
+};
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,41 +1,46 @@
-import { useEffect } from "react";
-import { apiRoot } from "../core/api/BuildClient";
+import { useEffect } from 'react';
+import {
+  apiRoot,
+  buildClientWithPasswordFlow,
+  customerApiRoot,
+} from '../core/api/BuildClient';
 
 const LoginPage = () => {
-
   const authUser = async () => {
-      apiRoot.login().post({
+    customerApiRoot
+      .login()
+      .post({
         body: {
           email: 'simon@gmail.com',
-          password: '150618'
-        }
-      }).execute()
-  }
+          password: '150618',
+        },
+      })
+      .execute();
+  };
 
-  // const createUser = async () => {
-  //   apiRoot.customers().post({
-  //     body: {
-  //       email: 'simon@gmail.com',
-  //       firstName: 'Dasha',
-  //       lastName: 'Belskaya',
-  //       password: '150618'
-  //     }
-  //   }).execute()
-  // }
+  const createUser = async () => {
+    apiRoot
+      .customers()
+      .post({
+        body: {
+          email: 'simon1@gmail.com',
+          firstName: 'Dasha',
+          lastName: 'Belskaya',
+          password: '150618',
+        },
+      })
+      .execute();
+  };
 
   const getProject = async () => {
     try {
-      const products = await apiRoot
-        .products()
-        .get()
-        .execute()
+      const products = await apiRoot.products().get().execute();
 
-      console.log('products', products)
-
+      console.log('products', products);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }
+  };
 
   // const getUserData = () => {
   //   apiRoot.me().get().execute()
@@ -43,30 +48,58 @@ const LoginPage = () => {
 
   const getCategories = async () => {
     try {
-      const categories = await apiRoot
-        .categories().get()
-        .execute()
+      const categories = await apiRoot.categories().get().execute();
 
-      console.log('categories', categories)
-
+      console.log('categories', categories);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
+  };
+
+  async function asyncEffect() {
+    buildClientWithPasswordFlow('simon@gmail.com', '150618');
+    const loggedIn1 = await customerApiRoot
+      .login()
+      .post({
+        body: {
+          email: 'simon@gmail.com',
+          password: '150618',
+        },
+      })
+      .execute();
+    console.log('logged in 1', loggedIn1);
+    const me1 = await customerApiRoot.me().get().execute();
+    console.log('me1', me1);
+
+    buildClientWithPasswordFlow('johndoe123@example.com', 'secret123');
+    const loggedIn2 = await customerApiRoot
+      .login()
+      .post({
+        body: {
+          email: 'johndoe123@example.com',
+          password: 'secret123',
+        },
+      })
+      .execute();
+    console.log('logged in 2', loggedIn2);
+    const me2 = await customerApiRoot.me().get().execute();
+    console.log('me2', me2);
   }
 
   useEffect(() => {
-    getProject()
-    getCategories()
-    authUser()
+    asyncEffect();
+    // getProject()
+    // getCategories()
+    // authUser()
     // getUserData()
     // createUser()
-  }, [])
+  }, []);
 
   return (
     <>
       <div>Project Details</div>
     </>
-  )
+  );
 };
 
 export default LoginPage;
